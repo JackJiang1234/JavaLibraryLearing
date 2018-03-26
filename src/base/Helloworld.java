670adfacb3b9477723f219b905de064aff28256c
jack <jjy_8888@163.com>
2018-03-26 23:10:07 +0800

    add test

---
+++ src/base/Helloworld.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package base;
+
+/**
+ * Description:
+ * Copyright (C) 
+ * Date: <br>
+ * @author jack 
+ * @version 1.0
+ */
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 56;
+		long bigValue = 9999999999999L;
+		int binVal = 0b1010111;
+	    System.out.println(a);
+	    System.out.println(bigValue);
+	    System.out.println(binVal);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/base/TestChar.java
@@ -0,0 +1,13 @@
+package base;
+
+public class TestChar {
+
+	public static void main(String[] args) {
+		char a = 'a';
+		//a = '\r';
+		char ch = '\u9999';
+		System.out.println(a);
+		System.out.println(ch);
+	}
+
+}
