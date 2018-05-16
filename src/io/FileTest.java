8f1a50e3b28d9465e6b9b58c5ea406dbb06874bb
jack <jjy_8888@163.com>
2018-05-17 07:57:51 +0800

    add io test

---
+++ src/io/FileTest.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class FileTest {
+
+	public static void main(String[] args) {
+		File f = new File("readme.md");
+		System.out.println(f.getName());
+		System.out.println(f.getAbsolutePath());
+		
+		System.out.println(File.pathSeparator);
+		System.out.println(File.separator);
+		System.out.println(Arrays.toString(File.listRoots())); 
+		
+		System.out.println(Paths.get("aa", "bb"));
+	}
+
+}
